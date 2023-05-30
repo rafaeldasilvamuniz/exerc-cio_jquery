64b88a3ef6c0a99c50e8dcbe9184c3abe1210f3e
rafaeldasilvamuniz <86721430+rafaeldasilvamuniz@users.noreply.github.com>
2023-05-30 08:49:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,48 +8,34 @@ $('#form-atividade').on('submit', function(e){
     e.preventDefault();
     adicionaLinha();
     atualizaTabela();
+    $('#risca').on('click', function(e){
+        $('li').css('text-decoration','line-through');
+     })
 
 })
    
-
+   
 
 
 function adicionaLinha() {    
       const inputNome = $('#nome').val();
 
     let linha = '<tr>';
-    linha += `<th><ul><li style="display: flex-wrap"><p>${inputNome}</p></li></ul></th>`;
-    
+    linha += `<th><ul><li style="display:flex-wrap">${inputNome}</li></ul><button type="submit" id="risca">OK</button></th>`;
+
     
     linha += '</tr>';
-
-    /*$(inputNome).on("click",function(){
-        $(this).toggleClass('text-decoration','line-through');
-    }
-    )*/
-    
-    
 
     linhas += linha;
 
     
 }
 
-function toggleRiscado() {
-    $(event.target).toggleClass('css');
-  }
-  
-
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
 
 }
-
-/*function myFunction(){
-    const x = $('#nome').val();
-    $(x).toggleClass('text-decoration','line-through');
-}*/
 
 
 
